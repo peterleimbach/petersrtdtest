bdf4f05c834c60e970d285d1b223a551e45c9e42
peterleimbach <peterleimbach@icloud.com>
2022-03-02 19:17:32 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,11 +17,12 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'myst_parser'
 ]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None)
 }
 intersphinx_disabled_domains = ['std']
 
